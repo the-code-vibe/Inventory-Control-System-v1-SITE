006bc6f461a882156966c36986a61f9905fc45a3
caioestevao1000 <caioestevao1000@gmail.com>
2025-04-02 23:31:59 -0300

    fix: :bug: correção de bug rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,14 +3,14 @@ import { createBrowserRouter } from "react-router-dom";
 // Imports pages
 import App from "./App";
 import Login from "./pages/Login";
-import PasswordRecovery from "./pages/passwordRecovery";
-import PasswordReset from "./pages/PasswordReset";
+// import PasswordRecovery from "./pages/passwordRecovery";
+// import PasswordReset from "./pages/PasswordReset";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/login", element: <Login /> },
-  { path: "/password-recovery", element: <PasswordRecovery /> },
-  { path: "/password-reset", element: <PasswordReset /> },
+  // { path: "/password-recovery", element: <PasswordRecovery /> },
+  // { path: "/password-reset", element: <PasswordReset /> },
 ]);
 
 export default router;
